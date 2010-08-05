fd0ab46a103f80a24878a574462cd8dec03242a1
cthulhu <cthulhu@test-laptop.(none)>
2010-08-05 10:03:39 +0300

    Added samples of topic exchange

--- ruby_chat.rb
+++ ruby_chat.rb
@@ -3,14 +3,25 @@ require 'mq'
 require 'json'
 
 AMQP.start(:host => '109.74.202.172') do
-  MQ.new.queue('auto').bind('simple_chat', :key => "simple_chat" ).subscribe{ |msg|
-    puts msg 
+  mq_all = MQ.new
+  mq_all.queue('all games topics').bind(mq_all.topic("games"), :key => "game_*" ).subscribe{ |info, msg|
+    puts "All games messages by key #{info.routing_key}, message #{msg}" 
+  }
+  mq_1 = MQ.new
+  mq_1.queue('game 1 topics').bind(mq_1.topic("games"), :key => "game_1" ).subscribe{ |msg|
+    puts "Game 1 message #{msg}" 
+  }
+  mq_2 = MQ.new
+  mq_2.queue('game 2 topics').bind(mq_2.topic("games"), :key => "game_2" ).subscribe{ |msg|
+    puts "Game 2 message #{msg}" 
   }
 
-  exchange = MQ::Exchange.new(MQ.new, :direct, "simple_chat" )
+  exchange = MQ::Exchange.new(MQ.new, :topic, "games" )
   #exchange = MQ::Exchange.new(MQ.new, :fanout, 'simple_chat', :key => 'simple_chat')
   EM.add_periodic_timer( 10 ){
-    exchange.publish( { "value" => "Auto puts from ruby client every 10 seconds: Time is #{Time.now.to_s}" }.to_json, :key => "simple_chat" )
+    ["game_1", "game_2"].each do |game|
+      exchange.publish( { "value" => "Game #{game} message" }.to_json, :key => game )
+    end
   }
   
 end
